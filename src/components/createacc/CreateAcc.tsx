870558da95c744fefc033c588782f85005690cac
Dawood Afzaal <dawoodafzaal13@gmail.com>
2023-09-11 22:27:33 -0600

    converted my css into tailwind

--- src/components/createacc/CreateAcc.tsx
+++ src/components/createacc/CreateAcc.tsx
@@ -1,4 +1,3 @@
-import "./acc.css"
 import {useState} from "react"
 import { useNavigate } from "react-router-dom";
 import CreateAccountInput from "./CreateAccInput";
@@ -9,6 +8,7 @@ export default function CreateAccount(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+    const className = "mb-[50px] w-[288px] h-[40px] rounded-md border-2 border-square-blue indent-2.5 text-[14px]";
 
     function handleUsernameChange(event : React.ChangeEvent<HTMLInputElement>){
         setUsername(event.target.value);
@@ -32,26 +32,30 @@ export default function CreateAccount(){
     return(
         <>
           <div className="flex justify-center">
-            <div className="acc-container">
-              <p>Create Account</p>
+            <div className="flex flex-col justify-center  mt-[200px] items-center w-[450px] h-[575px] rounded-md text-base border-2 border-square-blue">
+
+              <p className="mb-[50px] text-[16px]">Create Account</p>
               <CreateAccountInput
                 inputType="text"
                 placeholder="Enter email..."
                 onChange={handleEmailChange}
+                style={className}
               />
 
               <CreateAccountInput 
                 inputType="text"
                 placeholder="Enter username..."
                 onChange={handleUsernameChange}
+                style={className}
               />
 
               <CreateAccountInput
                 inputType="password"
                 placeholder="Enter password..."
                 onChange={handlePasswordChange}
+                style={className}
               />
-              <button onClick={handleAccountCreation}>Create!</button>
+              <button onClick={handleAccountCreation} className="mt-[25px] bg-square-blue w-[125px] h-[45px] rounded-md text-white">Create!</button>
             </div>
           </div>
         </>
--- src/components/createacc/CreateAccInput.tsx
+++ src/components/createacc/CreateAccInput.tsx
@@ -2,6 +2,7 @@ type myProps = {
     inputType: string,
     placeholder: string,
     onChange : (event: React.ChangeEvent<HTMLInputElement>) => void;
+    style : string
 }
 
 
@@ -9,7 +10,7 @@ export default function CreateAccountInput(props : myProps) {
     return(
         <>
           <div>
-            <input type={props.inputType} placeholder={props.placeholder} onChange={props.onChange}></input>
+            <input type={props.inputType} placeholder={props.placeholder} onChange={props.onChange} className={props.style}></input>
           </div>
         </>
     );
